7531b5f35ff2cb5e078571184e2ccd1b17ba1c2e
Maxime Chambreuil <maxime.chambreuil@savoirfairelinux.com>
2013-08-28 10:36:15 -0400

    [ADD] document_page_multi_company

---
+++ document_page_multi_company/__openerp__.py
@@ -0,0 +1,43 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#    
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2013 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.  
+#
+##############################################################################
+
+{
+    'name': 'Document Page Multi-Company',
+    'version': '1.0',
+    "author" : "Savoir-faire Linux",
+    "website" : "http://www.savoirfairelinux.com",
+    "license" : "AGPL-3",
+    'category': 'Knowledge Management',
+    'description': """
+This module adds a company field to document page and the multi-company rule.
+    """,
+    'depends': [
+        'document_page',
+        ],
+    'data': [
+        'security/document_page_security.xml',
+        'document_page_multi_company_view.xml',
+        ],
+    'installable': True,
+    'auto_install': False,
+    'images': [],
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
