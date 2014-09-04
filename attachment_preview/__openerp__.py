38f704666ab1d6d2427dcd150ed80247171a65d0
Holger Brunn <hbrunn@therp.nl>
2014-09-04 17:02:22 +0200

    [ADD] attachment_preview

---
+++ attachment_preview/__openerp__.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    This module copyright (C) 2014 Therp BV (<http://therp.nl>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    "name": "Preview attachments",
+    "version": "1.0",
+    "author": "Therp BV",
+    "license": "AGPL-3",
+    "complexity": "normal",
+    "description": """
+This addons allows to preview attachments supported by http://viewerjs.org.
+
+Currently, that's most Libreoffice files and PDFs.
+
+Acknowledgements
+================
+
+Addon icon courtesy of http://commons.wikimedia.org/wiki/Crystal_Clear
+    """,
+    "category" : "Knowledge Management",
+    "depends": [
+        'web'
+    ],
+    "data": [
+    ],
+    "js": [
+        'static/src/js/attachment_preview.js',
+    ],
+    "css": [
+        'static/src/css/attachment_preview.css',
+    ],
+    "qweb": [
+        'static/src/xml/attachment_preview.xml',
+    ],
+    "test": [
+    ],
+    "auto_install": False,
+    "installable": True,
+    "application": False,
+    "external_dependencies": {
+        'python': [],
+    },
+}
---
+++ attachment_preview/model/ir_attachment.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    This module copyright (C) 2014 Therp BV (<http://therp.nl>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+import collections
+import os.path
+from openerp.osv.orm import Model
+
+
+class IrAttachment(Model):
+    _inherit = 'ir.attachment'
+
+    def get_attachment_extension(self, cr, uid, ids, context=None):
+        result = {}
+        for this in self.browse(
+                cr, uid,
+                ids if isinstance(ids, collections.Iterable) else [ids],
+                context=context):
+            extension = ''
+            if this.datas_fname:
+                filename, extension = os.path.splitext(this.datas_fname)
+                extension = extension.lstrip('.')
+            if not extension:
+                #TODO: lookup the extension via mimetype
+                pass
+            result[this.id] = extension
+        return result if isinstance(ids, collections.Iterable) else result[ids]
