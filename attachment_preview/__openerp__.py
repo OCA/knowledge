2ead898a685baf0e0168ec1b9fecd4a15fb8c26d
Holger Brunn <hbrunn@therp.nl>
2015-01-15 11:17:18 +0100

    [ADD] preliminary port of attachment_preview

---
+++ attachment_preview/__openerp__.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    This module copyright (C) 2014 Therp BV (<http://therp.nl>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    "name": "Preview attachments",
+    "version": "1.1",
+    "author": "Therp BV",
+    "license": "AGPL-3",
+    "complexity": "normal",
+    "description": """
+This addon allows to preview attachments supported by http://viewerjs.org.
+
+Currently, that's most Libreoffice files and PDFs.
+
+For filetype recognition, you'll get the best results by installing
+``python-magic``.
+
+Acknowledgements
+================
+
+Addon icon courtesy of http://commons.wikimedia.org/wiki/Crystal_Clear
+    """,
+    "category": "Knowledge Management",
+    "depends": [
+        'web',
+        'document',
+    ],
+    "data": [
+        "view/attachment_preview.xml",
+    ],
+    "qweb": [
+        'static/src/xml/attachment_preview.xml',
+    ],
+    "test": [
+    ],
+    "auto_install": False,
+    "installable": True,
+    "application": False,
+    "external_dependencies": {
+        'python': [],
+    },
+}
