934ba97408117f641d3aa72b3b39e5f148801783
Holger Brunn <hbrunn@therp.nl>
2015-05-05 16:45:50 +0200

    [FIX] add OCA to authors

--- document_reindex/__openerp__.py
+++ document_reindex/__openerp__.py
@@ -21,7 +21,7 @@
 {
     "name": "Reindex documents",
     "version": "1.0",
-    "author": "Therp BV",
+    "author": "Therp BV, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Knowledge Management",
     "summary": "Reindex your already uploaded documents",
