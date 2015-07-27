678d20fb3e2700a2c1225c034e81a6acf47fe27a
Glen Sojo <glen.sojo@clearcorp.co.cr>
2015-07-27 15:11:28 -0600

    [FIX] - Fixed syntax error.

--- document_page_approval/document_page_approval.py
+++ document_page_approval/document_page_approval.py
@@ -59,13 +59,14 @@ class document_page_history_wkfl(models.Model):
             subtype_id = model_data_obj.get_object_reference(
                 cr, SUPERUSER_ID, 'mail', 'mt_comment')[1]
             message_obj.create(
-                cr, uid, {
-                    'res_id': page_history.page_id.id,
-                    'model': 'document.page',
-                    'subtype_id': subtype_id,
-                    'body': _('New version of the document %s'
-                              ' approved.') % page_history.page_id.name
-                          })
+                cr, uid,
+                {'res_id': page_history.page_id.id,
+                 'model': 'document.page',
+                 'subtype_id': subtype_id,
+                 'body': _('New version of the document %s'
+                           ' approved.') % page_history.page_id.name
+                 }
+            )
         return True
 
     def _can_user_approve_page(self):
