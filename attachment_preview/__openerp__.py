a74321600286016bb433053b00b1161475afd07c
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2015-10-09 10:00:24 +0200

    [UPD] prefix versions with 8.0

--- attachment_preview/__openerp__.py
+++ attachment_preview/__openerp__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 {
     "name": "Preview attachments",
-    "version": "1.1",
+    "version": "8.0.1.1.0",
     "author": "Therp BV,Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "complexity": "normal",
