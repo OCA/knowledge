e660a8620184c96e295e882f58d4b842e6017b67
Gervais Naoussi <gervaisnaoussi@gmail.com>
2015-10-22 02:37:37 -0400

    [MIG] document_page: Migration to 9.0

---
+++ document_page/README.rst
@@ -0,0 +1,68 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+=============
+Document Page
+=============
+
+This module allows you to write web pages for internal documentation.
+
+Installation
+============
+
+This module depends on module knowledge. So make sure to have it in your addons list.
+
+Configuration
+=============
+
+No configuration required
+
+Usage
+=====
+
+To use this module, you need to:
+
+* Go to Knowledge menu
+* Click on Categories to create the document's category you need with the template
+* Click on Pages to create pages and select the previous category to use the template
+
+
+.. image:: https://odoo-community.org/website/image/ir.attachment/5784_f2813bd/datas
+   :alt: Try me on Runbot
+   :target: https://runbot.odoo-community.org/runbot/118/9.0
+
+Known issues / Roadmap
+======================
+
+Bug Tracker
+===========
+
+Bugs are tracked on `GitHub Issues <https://github.com/OCA/Knowledge/issues>`_.
+In case of trouble, please check there if your issue has already been reported.
+If you spotted it first, help us smashing it by providing a detailed and welcomed feedback `here <https://github.com/OCA/knowledge/issues/new?body=module:%20document_page%0Aversion:%209.0%0A%0A**Steps%20to%20reproduce**%0A-%20...%0A%0A**Current%20behavior**%0A%0A**Expected%20behavior**>`_.
+
+Credits
+=======
+
+Contributors
+------------
+
+* Odoo SA <info@odoo.com>
+* Savoir-faire Linux <support@savoirfairelinux.com>
+* Gervais Naoussi <gervaisnaoussi@gmail.com>
+
+Maintainer
+----------
+
+.. image:: https://odoo-community.org/logo.png
+   :alt: Odoo Community Association
+   :target: https://odoo-community.org
+
+This module is maintained by the OCA.
+
+OCA, or the Odoo Community Association, is a nonprofit organization whose
+mission is to support the collaborative development of Odoo features and
+promote its widespread use.
+
+To contribute to this module, please visit http://odoo-community.org.
---
+++ document_page/models/document_page_history.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+import logging
+import difflib
+from openerp import models, fields, _
+
+_logger = logging.getLogger(__name__)
+
+
+class DocumentPageHistory(models.Model):
+    """This model is necessary to manage a document history."""
+
+    _name = "document.page.history"
+    _description = "Document Page History"
+    _order = 'id DESC'
+    _rec_name = "create_date"
+
+    page_id = fields.Many2one('document.page', 'Page')
+    summary = fields.Char('Summary', select=True)
+    content = fields.Text("Content")
+    create_date = fields.Datetime("Date")
+    create_uid = fields.Many2one('res.users', "Modified By")
+
+    def getDiff(self, v1, v2):
+        """Return the difference between two version of document version."""
+        text1 = self.browse(v1).content
+        text2 = self.browse(v2).content
+        line1 = line2 = ''
+        if text1:
+            line1 = text1.splitlines(1)
+        if text2:
+            line2 = text2.splitlines(1)
+        if (not line1 and not line2) or (line1 == line2):
+            return _('There are no changes in revisions.')
+        else:
+            diff = difflib.HtmlDiff()
+            return diff.make_table(
+                line1, line2,
+                "Revision-{}".format(v1),
+                "Revision-{}".format(v2),
+                context=True
+            )
