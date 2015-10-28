395f6363f26fcc0b8700f7be44b6cbfc62f9893e
Gervais Naoussi <gervaisnaoussi@gmail.com>
2015-10-28 10:55:22 -0400

    [MIG] document_page_approval: Migration to 9.0
    * Lint error corrected
    * test improved
    * lint error corrected in test file
    * readme corrected
    * test improved
    * test of document page history workflow added
    * CI error corrected
    * dreipst comment
    * Migration to new api improved in model's file and data tag remove in xml file
    * code totally migrate to odoo 9.0 api
    * test bug fixed
    * test error fixed
    * Lint error improved

--- document_page_approval/__init__.py
+++ document_page_approval/__init__.py
@@ -1,4 +1,4 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 ##############################################################################
 #
 #    OpenERP, Open Source Management Solution
@@ -18,4 +18,4 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-from . import document_page_approval
+from . import models
