82b0942a5d6d91f6e8c45ab5e94b6ffdb27e054e
Gervais Naoussi <gervaisnaoussi@gmail.com>
2015-10-28 10:55:22 -0400

    [MIG] document_page_approval: Migration to 9.0
    * Lint error corrected
    * test improved
    * lint error corrected in test file
    * readme corrected
    * test improved
    * test of document page history workflow added
    * CI error corrected
    * dreipst comment
    * Migration to new api improved in model's file and data tag remove in xml file
    * code totally migrate to odoo 9.0 api
    * test bug fixed
    * test error fixed
    * Lint error improved

--- document_page_approval/models/document_page_history_workflow.py
+++ document_page_approval/models/document_page_history_workflow.py
@@ -1,4 +1,4 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 ##############################################################################
 #
 #    OpenERP, Open Source Management Solution
@@ -22,54 +22,51 @@
 from datetime import datetime
 from openerp.tools.translate import _
 from openerp.tools import DEFAULT_SERVER_DATETIME_FORMAT
-from openerp import models, fields, SUPERUSER_ID
+from openerp import models, fields, api
 
 
-class document_page_history_wkfl(models.Model):
+class DocumentPageHistoryWorkflow(models.Model):
+    """Useful to manage edition's workflow on a document."""
+
     _inherit = 'document.page.history'
 
-    def page_approval_draft(self, cr, uid, ids, context=None):
-        self.write(cr, uid, ids, {'state': 'draft'})
-        template_id = self.pool.get('ir.model.data').get_object_reference(
-            cr, uid,
-            'document_page_approval',
-            'email_template_new_draft_need_approval')[1]
-        for page in self.browse(cr, uid, ids, context=context):
+    @api.multi
+    def page_approval_draft(self):
+        """Set a document state as draft and notified the reviewers."""
+        self.write({'state': 'draft'})
+        template = self.env.ref(
+            'document_page_approval.email_template_new_draft_need_approval')
+        for page in self:
             if page.is_parent_approval_required:
-                self.pool.get('email.template').send_mail(
-                    cr,
-                    uid,
-                    template_id,
-                    page.id,
-                    force_send=True
-                )
+                template.send_mail(page.id, force_send=True)
         return True
 
-    def page_approval_approved(self, cr, uid, ids, context=None):
-        model_data_obj = self.pool.get('ir.model.data')
-        message_obj = self.pool.get('mail.message')
-        self.write(cr, uid, ids, {
+    @api.multi
+    def page_approval_approved(self):
+        """Set a document state as approve."""
+        message_obj = self.env['mail.message']
+        self.write({
             'state': 'approved',
             'approved_date': datetime.now().strftime(
                 DEFAULT_SERVER_DATETIME_FORMAT),
-            'approved_uid': uid
-        }, context=context)
+            'approved_uid': self.env.uid
+        })
         # Notify followers a new version is available
-        for page_history in self.browse(cr, uid, ids, context=context):
-            subtype_id = model_data_obj.get_object_reference(
-                cr, SUPERUSER_ID, 'mail', 'mt_comment')[1]
+        for page_history in self:
+            subtype = self.env.ref('mail.mt_comment')
             message_obj.create(
-                cr, uid,
                 {'res_id': page_history.page_id.id,
                  'model': 'document.page',
-                 'subtype_id': subtype_id,
+                 'subtype_id': subtype.id,
                  'body': _('New version of the document %s'
                            ' approved.') % page_history.page_id.name
                  }
             )
         return True
 
+    @api.multi
     def _can_user_approve_page(self):
+        """Check if a user cas approve the page."""
         user = self.env.user
         for page in self:
             page.can_user_approve_page = page.can_user_approve_this_page(
@@ -78,6 +75,7 @@ class document_page_history_wkfl(models.Model):
             )
 
     def can_user_approve_this_page(self, page, user):
+        """Check if a user can approved the page."""
         if page:
             res = page.approver_gid in user.groups_id
             res = res or self.can_user_approve_this_page(page.parent_id, user)
@@ -85,13 +83,16 @@ class document_page_history_wkfl(models.Model):
             res = False
         return res
 
+    @api.multi
     def get_approvers_guids(self):
+        """Return the approvers group."""
         res = {}
         for page in self:
             res[page.id] = self.get_approvers_guids_for_page(page.page_id)
         return res
 
     def get_approvers_guids_for_page(self, page):
+        """Return the approvers group for a page."""
         if page:
             if page.approver_gid:
                 res = [page.approver_gid.id]
@@ -103,7 +104,9 @@ class document_page_history_wkfl(models.Model):
 
         return res
 
+    @api.multi
     def _get_approvers_email(self):
+        """Get the approvers email."""
         for page in self:
             emails = ''
             guids = self.get_approvers_guids()
@@ -126,7 +129,9 @@ class document_page_history_wkfl(models.Model):
 
             page.get_approvers_email = emails[:-1]
 
+    @api.multi
     def _get_page_url(self):
+        """Get the page url."""
         for page in self:
             base_url = self.env['ir.config_parameter'].get_param(
                 'web.base.url',
@@ -174,101 +179,4 @@ class document_page_history_wkfl(models.Model):
         compute=_get_page_url,
         string="URL",
         store=False
-    )
-
-
-class document_page_approval(models.Model):
-    _inherit = 'document.page'
-
-    def _get_display_content(self):
-        for page in self:
-            content = ""
-            if page.type == "category":
-                content = self._get_page_index(page, link=False)
-            else:
-                history = self.env['document.page.history']
-                if self.is_approval_required(page):
-                    history_ids = history.search(
-                        [
-                            ('page_id', '=', page.id),
-                            ('state', '=', 'approved')
-                        ],
-                        limit=1,
-                        order='create_date DESC'
-                    )
-                    content = history_ids.content
-                else:
-                    content = page.content
-            page.display_content = content
-
-    def _get_approved_date(self):
-        for page in self:
-            approved_date = False
-            if self.is_approval_required(page):
-                history = self.env['document.page.history']
-                history_ids = history.search(
-                    [
-                        ('page_id', '=', page.id),
-                        ('state', '=', 'approved')
-                    ],
-                    limit=1,
-                    order='create_date DESC'
-                )
-                approved_date = history_ids.approved_date
-            page.approved_date = approved_date
-
-    def _get_approved_uid(self):
-        for page in self:
-            approved_uid = False
-            if self.is_approval_required(page):
-                history = self.env['document.page.history']
-                history_ids = history.search(
-                    [
-                        ('page_id', '=', page.id),
-                        ('state', '=', 'approved')
-                    ],
-                    limit=1,
-                    order='create_date DESC'
-                )
-                approved_uid = history_ids.approved_uid.id
-            page.approved_uid = approved_uid
-
-    def _is_parent_approval_required(self):
-        for page in self:
-            page.is_parent_approval_required = self.is_approval_required(page)
-
-    def is_approval_required(self, page):
-        if page:
-            res = page.approval_required
-            res = res or self.is_approval_required(page.parent_id)
-        else:
-            res = False
-        return res
-
-    display_content = fields.Text(
-        compute=_get_display_content,
-        string='Displayed Content'
-    )
-
-    approved_date = fields.Datetime(
-        compute=_get_approved_date,
-        string="Approved Date"
-    )
-
-    approved_uid = fields.Many2one(
-        'res.users',
-        compute=_get_approved_uid,
-        string="Approved By",
-    )
-
-    approval_required = fields.Boolean("Require approval")
-
-    is_parent_approval_required = fields.Boolean(
-        compute=_is_parent_approval_required,
-        string="parent approval"
-    )
-
-    approver_gid = fields.Many2one(
-        "res.groups",
-        "Approver group"
     )
