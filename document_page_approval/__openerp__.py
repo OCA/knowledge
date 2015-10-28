8af4502acb618b3fbfaaacf0740def95f0f93221
Gervais Naoussi <gervaisnaoussi@gmail.com>
2015-10-28 10:55:22 -0400

    [MIG] document_page_approval: Migration to 9.0
    * Lint error corrected
    * test improved
    * lint error corrected in test file
    * readme corrected
    * test improved
    * test of document page history workflow added
    * CI error corrected
    * dreipst comment
    * Migration to new api improved in model's file and data tag remove in xml file
    * code totally migrate to odoo 9.0 api
    * test bug fixed
    * test error fixed
    * Lint error improved

--- document_page_approval/__openerp__.py
+++ document_page_approval/__openerp__.py
@@ -1,4 +1,4 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 ##############################################################################
 #
 #    OpenERP, Open Source Management Solution
@@ -21,36 +21,20 @@
 
 {
     'name': 'Document Page Approval',
-    'version': '8.0.1.0.0',
+    'version': '9.0.1.0.0',
     "author": "Savoir-faire Linux,Odoo Community Association (OCA)",
     "website": "http://www.savoirfairelinux.com",
     "license": "AGPL-3",
     'category': 'Knowledge Management',
-    'description': """
-This module adds a workflow to approve page modification and show the approved
-version by default.
-
-Scenario
-========
-
-* Set a valid email address on the company settings.
-* Create a new page category and set an approver group. Make sure users
-  belonging to that group have valid email addresses.
-* Create a new page and choose the previously created category.
-* A notification is sent to the group with a link to the page history to
-  review.
-* Depending on the review, the page history is approved or not.
-* Users reading the page see the last approved version.
-    """,
     'depends': [
         'knowledge',
         'document_page',
-        'email_template',
+        'mail',
     ],
     'data': [
         'data/email_template.xml',
-        'document_page_wkfl.xml',
-        'document_page_view.xml',
+        'workflows/document_page_approval.xml',
+        'views/document_page_approval.xml',
         'security/document_page_security.xml',
         'security/ir.model.access.csv',
     ],
---
+++ document_page_approval/tests/test_document_page_approval.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from openerp.tests import common
+
+
+class TestDocumentPageApproval(common.TransactionCase):
+    """Test document page approval model."""
+
+    def test_get_display_content(self):
+        """Test page display content."""
+        # Check content of a category
+        category = self.env['document.page'].search([
+            ('name', '=', 'OpenERP Features')
+        ])
+
+        self.assertIsNotNone(category.display_content, 'a category')
+
+        # Check content of a page
+        pages = self.env['document.page'].search([
+            ('parent_id', '=', category.id)
+        ])
+        page = pages[0]
+        self.assertIsNotNone(page.display_content, 'Page content')
+
+        # Check if approval is required
+        self.assertTrue(page.is_approval_required(page) ==
+                        category.approval_required)
+
+        # Check content of an approval page
+        page.approval_required = True
+
+        self.assertIsNotNone(page.display_content, 'Page content')
+
+        # Check if approval is required
+        self.assertTrue(page.is_approval_required(page))
+
+        # Check if parent approval is required
+        self.assertTrue(page.is_parent_approval_required)
