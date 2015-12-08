37277551c7043f6b0ebf5aee0acb061aa52a3284
Gervais Naoussi <gervaisnaoussi@gmail.com>
2015-12-08 13:02:51 +0100

    pylint error in test file corrected

--- document_page/tests/test_document_page_create_menu.py
+++ document_page/tests/test_document_page_create_menu.py
@@ -1,4 +1,4 @@
-# -*- coding:utf-8 -*-
+# -*- coding: utf-8 -*-
 
 from openerp.tests import common
 
@@ -8,13 +8,14 @@ class TestDocumentPageCreateMenu(common.TransactionCase):
 
     def test_page_menu_creation(self):
         """Test page menu creation."""
-
         menu_parent = self.env.ref('base.menu_base_partner')
 
         menu_created = self.env['document.page.create.menu'].create({
-             'menu_name': 'Wiki Test menu',
-             'menu_parent_id': menu_parent.id
-        })
+             'menu_name': 'Wiki Test menu', 'menu_parent_id': menu_parent.id})
 
-        menu = self.env['document.page.create.menu'].search([('id', '=', menu_created.id)])
-        menu.with_context(active_id=[self.ref('document_page.demo_page1')]).document_page_menu_create()
+        menu = self.env['document.page.create.menu'].search(
+            [('id', '=', menu_created.id)]
+        )
+        menu.with_context(
+            active_id=[self.ref('document_page.demo_page1')]
+        ).document_page_menu_create()
