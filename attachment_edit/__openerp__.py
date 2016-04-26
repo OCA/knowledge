3f5a0e8adc7defe37296c4fd80c068ad814ee79b
Holger Brunn <hbrunn@therp.nl>
2016-04-27 00:03:09 +0200

    [ADD] OCA as author

--- attachment_edit/__openerp__.py
+++ attachment_edit/__openerp__.py
@@ -4,7 +4,7 @@
 {
     "name": "Edit attachments",
     "version": "8.0.1.0.0",
-    "author": "Therp BV",
+    "author": "Therp BV,Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Knowledge Management",
     "summary": "Edit attachments after upload",
