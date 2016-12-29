8fab04e6b65e28171df22b93a4ebe4a354a2a456
Leonardo Donelli <learts92@gmail.com>
2016-12-29 12:17:53 +0100

    Add module document_type

---
+++ document_type/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 MONK Software
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Document Types"
+    "version": "10.0.1.0.0",
+    "author": "MONK Software, Odoo Community Association (OCA)",
+    "category": "Knowledge",
+    "license": "AGPL-3",
+    "website": "https://odoo-community.org/",
+    "depends": ["knowledge"],
+    "data": [],
+    'installable': True,
+    "auto_install": False,
+}
