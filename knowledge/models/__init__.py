1c67e707770164e15537662531804777d803de2a
Maxime Chambreuil <mchambreuil@ursainfosystems.com>
2017-01-05 12:53:38 -0600

    [FIX] knowledge: Several things
    * Copyright and various stuff
    * Contributors
    * External ID not found in the system: base.group_document_user

--- knowledge/models/__init__.py
+++ knowledge/models/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2016 MONK Software
+# Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import ir_attachment
