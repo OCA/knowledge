c1c17af96469db9a2452274185974ce857e19bdf
Maxime Chambreuil <mchambreuil@ursainfosystems.com>
2017-02-02 06:30:30 -0800

    [MIG] document_page: Migration to version 10.0 (#120)
    * [MIG] Migration to version 10.0
    * [FIX] Comments and CI errors
    * [FIX] ValueError: External ID not found in the system: base.menu_base_partner
    * [FIX] ValueError: Wrong value for ir.actions.act_window.target: 'inlineview'
    * [FIX] Based on @lasley comments
    * [FIX] External ID not found in the system: base.group_document_user

--- document_page/__manifest__.py
+++ document_page/__manifest__.py
@@ -1,32 +1,21 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 
 {
     'name': 'Document Page',
-    'version': '9.0.1.0.1',
+    'version': '10.0.1.0.0',
     'category': 'Knowledge Management',
     'author': 'OpenERP SA, Odoo Community Association (OCA)',
-    'images': ['images/category_list.png', 'images/create_category.png',
-                'images/page_list.png', 'images/create_page.png',
-                'images/customer_invoice.jpeg', 'images/page_history.png'],
+    'images': [
+        'images/category_list.png',
+        'images/create_category.png',
+        'images/page_list.png',
+        'images/create_page.png',
+        'images/customer_invoice.jpeg',
+        'images/page_history.png',
+    ],
     'website': 'http://www.openerp.com/',
     'license': 'AGPL-3',
     'depends': [
@@ -39,11 +28,9 @@
         'views/document_page.xml',
         'security/document_page_security.xml',
         'security/ir.model.access.csv',
+        'data/document_page.xml',
     ],
     'demo': [
         'demo/document_page.xml'
     ],
-    'installable': False,
-    'auto_install': False,
-    'css': ['static/src/css/document_page.css'],
 }
