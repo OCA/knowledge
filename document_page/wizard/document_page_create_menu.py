b862c87e8cb23eae382b487d0f5107185e464ec5
Maxime Chambreuil <mchambreuil@ursainfosystems.com>
2017-02-02 06:30:30 -0800

    [MIG] document_page: Migration to version 10.0 (#120)
    * [MIG] Migration to version 10.0
    * [FIX] Comments and CI errors
    * [FIX] ValueError: External ID not found in the system: base.menu_base_partner
    * [FIX] ValueError: Wrong value for ir.actions.act_window.target: 'inlineview'
    * [FIX] Based on @lasley comments
    * [FIX] External ID not found in the system: base.group_document_user

--- document_page/wizard/document_page_create_menu.py
+++ document_page/wizard/document_page_create_menu.py
@@ -1,24 +1,8 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-from openerp import models, fields, api
+# Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
 
 
 class DocumentPageCreateMenu(models.TransientModel):
@@ -68,7 +52,7 @@ class DocumentPageCreateMenu(models.TransientModel):
             'res_model': 'document.page',
             'view_id': view_id,
             'type': 'ir.actions.act_window',
-            'target': 'inlineview',
+            'target': 'inline',
         }
         value['domain'] = "[('parent_id','=',%d)]" % (page.id)
         value['res_id'] = page.id
--- document_page/wizard/document_page_show_diff.py
+++ document_page/wizard/document_page_show_diff.py
@@ -1,25 +1,9 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-from openerp import models, fields, _
-from openerp import exceptions
+# Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import exceptions, fields, models
+from odoo.tools.translate import _
 
 
 class DocumentPageShowDiff(models.TransientModel):
