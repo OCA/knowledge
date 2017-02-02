2bd6f94dab56a7b8d15565160df6702f60a3ea3e
Leonardo Donelli <learts92@gmail.com>
2017-02-02 11:15:33 +0100

    Add documents count smart buttons for document type

--- document_type/models/document_type.py
+++ document_type/models/document_type.py
@@ -9,6 +9,11 @@ class DocumentType(models.Model):
     ir_model_id = fields.Many2one('ir.model', string='Object')
     document_ids = fields.One2many(
         'ir.attachment', 'document_type_id', string='Documents')
+    documents_count = fields.Integer(compute='_compute_documents_count')
+
+    def _compute_documents_count(self):
+        for record in self:
+            record.documents_count = len(record.document_ids)
 
 
 class IrAttachment(models.Model):
