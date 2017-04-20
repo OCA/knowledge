52bc8fcf9d2c7189fc004f84bb5efe86ef237f5f
Maxime Chambreuil <mchambreuil@ursainfosystems.com>
2017-04-20 16:18:37 -0500

    Revert "[FIX] #109"
    This reverts commit b1ded5e3e6b4610c94c25aa990074034091de8fb.

--- document_page/models/document_page.py
+++ document_page/models/document_page.py
@@ -76,26 +76,26 @@ class DocumentPage(models.Model):
 
     create_date = fields.Datetime(
         "Created on",
-        index=True,
+        select=True,
         readonly=True
     )
 
     create_uid = fields.Many2one(
         'res.users',
         'Author',
-        index=True,
+        select=True,
         readonly=True
     )
 
     write_date = fields.Datetime(
         "Modification Date",
-        index=True,
+        select=True,
         readonly=True)
 
     write_uid = fields.Many2one(
         'res.users',
         "Last Contributor",
-        index=True,
+        select=True,
         readonly=True
     )
 
