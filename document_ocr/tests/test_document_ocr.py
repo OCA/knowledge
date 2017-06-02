125d76d37d860271eac3b770a495d7fa40a1ce35
Carlos Almeida <carlos.almeida@tkobr.com>
2017-06-02 10:28:57 +0100

    Fix attachment import

--- document_ocr/tests/test_document_ocr.py
+++ document_ocr/tests/test_document_ocr.py
@@ -5,7 +5,7 @@
 from StringIO import StringIO
 
 from PIL import Image, ImageDraw, ImageFont
-from document_ocr.models.ir_attachment import _MARKER_PHRASE
+from ir_attachment import _MARKER_PHRASE
 from odoo.tests.common import TransactionCase
 
 
