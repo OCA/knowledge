1b56ee20bd05de64b774258e52b69059fc260ddb
Giovanni Francesco Capalbo <giovanni@therp.nl>
2017-09-22 12:36:40 +0200

    first version

--- document_definition/__openerp__.py
+++ document_definition/__openerp__.py
@@ -2,31 +2,17 @@
 # © 2017 Therp BV <http://therp.nl>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    "name": "Term",
+    "name": "Terms and Definitions",
     "version": "8.0.1.0.0",
     "author": "Therp BV,Odoo Community Association (OCA)",
     "license": "AGPL-3",
-    "category": "",
-    "summary": "",
+    "category": "Knowledge",
     "depends": [
+        'knowledge',
     ],
     "data": [
         'views/templates.xml',
         'security/ir.model.access.csv',
     ],
-    "qweb": [
-    ],
-    "test": [
-    ],
-    "images": [
-    ],
-    "pre_init_hook": False,
-    "post_init_hook": False,
-    "uninstall_hook": False,
-    "auto_install": False,
     "installable": True,
-    "application": False,
-    "external_dependencies": {
-        'python': [],
-    },
 }
--- document_definition/models/document_definition.py
+++ document_definition/models/document_definition.py
@@ -4,8 +4,36 @@
 from openerp import api, fields, models
 
 
-class document_definition(models.Model):
-    _inherit = 'document_definition'
-    _name = 'document_definition'
-    _description = ''
+class DocumentDefinition(models.Model):
+    _name = 'document.definition'
 
+    term = fields.Char(help='Term to define', required=True)
+    definition = fields.Text(help='Definition of term', required=True)
+
+    # stored compute field used for making the letter grouping
+    # need to be stored in order to use grouping
+    first_letter = fields.Char()
+
+    def _get_first_letter(self, text):
+        # if somehow the user starts with a blank space we trim it
+        return text.lstrip()[:1].upper() or 'Blank Term'
+
+    @api.model
+    def create(self, vals):
+        vals['first_letter'] = self._get_first_letter(vals['term'])
+        res = super(DocumentDefinition, self).create(vals=vals)
+        return res
+
+    @api.multi
+    def write(self, vals):
+        for this in self:
+            if vals.get('term'):
+                vals['first_letter'] = this._get_first_letter(vals['term'])
+            res = super(DocumentDefinition, this).write(vals=vals)
+        return res
+
+    # constraint of uniqueness on term, two terms can't have different
+    # definitions
+
+    _sql_constraints = [('term_uniq', 'UNIQUE(term)',
+                         'Every term must be unique')]
