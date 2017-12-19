15796c87b85b8d05db7123734e0a7bf02bca37ca
fayez <customvivvo@gmail.com>
2017-12-19 14:45:31 +0100

    knowledge: Migration to 11.0

--- document_knowledge/__manifest__.py
+++ document_knowledge/__manifest__.py
@@ -1,9 +1,8 @@
-# -*- coding: utf-8 -*-
 # Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Knowledge Management System",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "author": "OpenERP SA, MONK Software, Odoo Community Association (OCA)",
     "category": "Knowledge",
     "license": "AGPL-3",
