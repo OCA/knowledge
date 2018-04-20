bbffafdae62cba9385f1d6a1c949a000f7b94a1a
Iván Todorovich <ivan.todorovich@gmail.com>
2018-04-20 10:42:49 -0300

    [document_page_approval] Rename classes and files to match model names, and removed workflow engine.

--- document_page_approval/models/document_page.py
+++ document_page_approval/models/document_page.py
@@ -7,7 +7,7 @@ from odoo import api, fields, models
 from ast import literal_eval
 
 
-class DocumentPageApproval(models.Model):
+class DocumentPage(models.Model):
     """Useful to know the state of a document."""
 
     _inherit = 'document.page'
@@ -122,8 +122,8 @@ class DocumentPageApproval(models.Model):
 
     @api.multi
     def _create_history(self, vals):
-        res = super(DocumentPageApproval, self)._create_history(vals)
-        res.signal_workflow('document_page_auto_confirm')
+        res = super(DocumentPage, self)._create_history(vals)
+        res.action_to_approve()
 
     @api.multi
     def action_changes_pending_approval(self):
