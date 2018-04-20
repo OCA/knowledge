f9bc09d56fc5ee6f87ce7b8d3dc60c60d0ff3370
Iván Todorovich <ivan.todorovich@gmail.com>
2018-04-20 10:42:49 -0300

    [FIX+IMP] document_page_approval: * Rename classes and files to match model names, and removed workflow engine. * Some fields should be readonly if not in draft. * Allow Knowledge / Manager to edit previous history names and summary * Improve ux by adding user_has_drafts to document pages. * Changes because of implementation of active field on document_page * Add ir.rules for document.page.history * Allow to quickly cancel&draft from 'to approve' state * FIX BUG: am_i_approver was being run as sudo(), hence always giving the user Approving rights. * FIX hooks   Because of default='draft' on the state field, state IS NULL wasn't selecting anything.   Left the condition in case this is changed in the future.

--- document_page_approval/__manifest__.py
+++ document_page_approval/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Document Page Approval',
-    'version': '11.0.1.0.0',
+    'version': '11.0.2.0.0',
     "author": "Savoir-faire Linux, Odoo Community Association (OCA)",
     "website": "http://www.savoirfairelinux.com",
     "license": "AGPL-3",
