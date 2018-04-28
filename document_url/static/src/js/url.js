8b22a713f0ef5d6e1c9326001c9b1a77713bbff2
ernesto <ernesto.tejeda87@gmail.com>
2018-04-28 09:02:51 -0400

    [MIG] document_url: Migration to 11.0

--- document_url/static/src/js/url.js
+++ document_url/static/src/js/url.js
@@ -3,39 +3,59 @@
  * © 2016 ACSONE SA/NV (<http://acsone.eu>)
  * License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
  */
-odoo.define('document_url', function(require) {
+odoo.define('document_url', function (require) {
 
     var core = require('web.core');
     var Sidebar = require('web.Sidebar');
     var ActionManager = require('web.ActionManager');
+    var Context = require('web.Context');
+    var pyeval = require('web.pyeval');
 
     var _t = core._t,
         QWeb = core.qweb;
 
     Sidebar.include({
-        redraw: function() {
+        _redraw: function () {
             var self = this;
             this._super.apply(this, arguments);
             self.$el.find("a[href]").attr('target', '_blank');
-            self.$el.find('.oe_sidebar_add_attachment, .o_sidebar_add_attachment').after(QWeb.render('AddUrlDocumentItem', {widget: self}))
-            self.$el.find('.oe_sidebar_add_url').on('click', function (e) {
+            self.$el
+                .find('.oe_sidebar_add_attachment, .o_sidebar_add_attachment')
+                .after(QWeb.render('AddUrlDocumentItem', {widget: self}))
+            self.$el.find('.o_sidebar_add_url').on('click', function (e) {
                 self.on_url_doc();
             });
         },
-        on_url_doc: function() {
+        on_url_doc: function (event) {
+
             var self = this;
+            var env = self.env
             var view = self.getParent();
-            var ids = ( view.fields_view.type != "form" )? view.groups.get_selection().ids : [ view.datarecord.id ];
-            if( !_.isEmpty(ids) ){
-                view.sidebar_eval_context().done(function (context) {
-                    self.rpc("/web/action/load", { action_id: "document_url.action_ir_attachment_add_url" }).done(function(result) {
-                        self.getParent().do_action(result, {
-                            additional_context: {
-                                'active_ids': ids,
-                                'active_id': [ids[0]],
-                                'active_model': view.dataset.model,
-                            },
-                        }); 
+            var ids = self.env.activeIds;
+            if (!_.isEmpty(ids)) {
+                var activeIdsContext = {
+                    active_id: env.activeIds[0],
+                    active_ids: env.activeIds,
+                    active_model: env.model,
+                };
+                if (env.domain) {
+                    activeIdsContext.active_domain = env.domain;
+                }
+                var context = new Context(env.context, activeIdsContext)
+                context = pyeval.eval('context', context);
+                self._rpc({
+                    route: "/web/action/load",
+                    params: {
+                        action_id: "document_url.action_ir_attachment_add_url",
+                        context: context,
+                    },
+                }).done(function (result) {
+                    self.getParent().do_action(result, {
+                        additional_context: {
+                            'active_ids': ids,
+                            'active_id': [ids[0]],
+                            'active_model': env.model,
+                        },
                     });
                 });
             }
