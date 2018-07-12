b930f87df7152a221fef4af975f0ab1cdab46ca8
Holger Brunn <hbrunn@therp.nl>
2018-07-12 16:28:49 +0200

    [MIG] document_page_partner_id

---
+++ document_page_partner_id/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2015-18 Therp BV <https://therp.nl>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Link to a partner in document pages",
+    "version": "10.0.1.0.0",
+    "author": "Therp BV,Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "category": "Knowledge Management",
+    "summary": "Allows to link doucment pages to a partner",
+    "depends": [
+        'document_page',
+    ],
+    "data": [
+        "views/document_page.xml",
+    ],
+    "installable": True,
+}
