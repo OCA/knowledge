d719d6a57fa88cf85db716368ee3341a301db290
Gabriela Mogollon <gmogollon@vauxoo.com>
2018-07-23 19:42:34 +0000

    [FIX] document_page: fix report
    - fix references: report module is now web module
    
    - display_* fields are removed in this version

--- document_page/__manifest__.py
+++ document_page/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     'name': 'Document Page',
-    'version': '11.0.2.1.0',
+    'version': '11.0.2.2.0',
     'category': 'Knowledge Management',
     'author': 'OpenERP SA, Odoo Community Association (OCA)',
     'images': [
