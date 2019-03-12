ad253a7f97a90c5ea8e2a712750303d8d0932ca0
mreficent <miquel.raich@eficent.com>
2019-03-12 12:47:05 +0100

    [MIG] knowledge: Migration to 12.0

--- document_knowledge/__manifest__.py
+++ document_knowledge/__manifest__.py
@@ -1,8 +1,8 @@
 # Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    "name": "Knowledge Management System",
-    "version": "11.0.3.0.0",
+    "name": "Knowledge",
+    "version": "12.0.1.0.0",
     "author": "OpenERP SA,"
               "MONK Software, "
               "Tecnativa, "
