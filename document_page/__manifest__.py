0983cbc937bd91018c8bf18f5f69e897f22677db
oca-travis <oca+oca-travis@odoo-community.org>
2019-07-16 07:47:43 +0000

    [UPD] Update document_page.pot
    [UPD] README.rst
    
    document_page 12.0.1.1.0
    
    Update translation files
    
    Updated by "Update PO files to match POT (msgmerge)" hook in Weblate.
    
    Translation: knowledge-12.0/knowledge-12.0-document_page
    Translate-URL: https://translation.odoo-community.org/projects/knowledge-12-0/knowledge-12-0-document_page/
    
    [UPD] README.rst

--- document_page/__manifest__.py
+++ document_page/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     'name': 'Document Page',
-    'version': '12.0.1.0.0',
+    'version': '12.0.1.1.0',
     'category': 'Knowledge Management',
     'author': 'OpenERP SA, Odoo Community Association (OCA)',
     'images': [
