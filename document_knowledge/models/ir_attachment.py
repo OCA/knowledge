57013f7e91d4b62107d7140de909f188f054b064
ernesto <ernesto.tejeda87@gmail.com>
2019-11-05 17:48:10 -0500

    [IMP] knowledge: black, isort

--- document_knowledge/models/ir_attachment.py
+++ document_knowledge/models/ir_attachment.py
@@ -5,7 +5,7 @@ from odoo import fields, models
 
 
 class IrAttachment(models.Model):
-    _inherit = 'ir.attachment'
+    _inherit = "ir.attachment"
 
     # Add index to res_model because filtering on it is a common use case
     res_model = fields.Char(index=True)
