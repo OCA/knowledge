8904d50e67e7ffe234e6e1b0892f0429abdea2a8
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2019-11-29 14:31:58 +0100

    add option to create a new page from a non-existing reference

--- document_page_reference/models/document_page.py
+++ document_page_reference/models/document_page.py
@@ -46,6 +46,11 @@ class DocumentPage(models.Model):
 
     _inherit = 'document.page'
 
+    @api.model
+    def default_get(self, fields):
+        rec = super(DocumentPage, self).default_get(fields)
+        return rec
+
     reference = fields.Char(
         help="Used to find the document, it can contain letters, numbers and _"
     )
@@ -74,21 +79,25 @@ class DocumentPage(models.Model):
         document = self.search([('reference', '=', code)])
         if document:
             return document
-        return False
+        else:
+            return self.env[self._name]
 
     def get_reference(self, code):
         element = self._get_document(code)
-        if not element:
-            return code
         if self.env.context.get('raw_reference', False):
             return html_escape(element.display_name)
-        text = '<a href="#" class="oe_direct_line" ' \
-               'name="%s" data-id="%s">%s</a>'
-        return text % (
+        text = """<a href="#" class="oe_direct_line" 
+        data-oe-model="%s" data-oe-id="%s" name="%s">%s</a>        
+        """
+        if not element:
+            text = '<i>%s</i>' % text
+        res = text % (
             element._name,
-            element.id,
-            html_escape(element.display_name)
+            element and element.id or '',
+            code,
+            html_escape(element.display_name or code),
         )
+        return res
 
     def _get_template_variables(self):
         return {'ref': self.get_reference}
