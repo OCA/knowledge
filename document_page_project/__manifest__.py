fc18e3d03277d00e1a3a2f6046ce635f081ac19a
Joan Sisquella <joan.sisquella@forgeflow.com>
2020-01-15 12:40:00 +0100

    [MIG] document_page_project: Migration to 13.0

--- document_page_project/__manifest__.py
+++ document_page_project/__manifest__.py
@@ -1,14 +1,13 @@
-# Copyright 2019 Eficent Business and IT Consulting Services S.L.
-#   (http://www.eficent.com) - Lois Rilo
+# Copyright 2019 ForgeFlow S.L. (https://www.forgeflow.com) - Lois Rilo
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Document Page Project",
     "summary": "This module links document pages to projects",
-    "version": "12.0.1.0.0",
-    "development_status": "Beta",
+    "version": "13.0.1.0.0",
+    "development_status": "Production/Stable",
     "category": "Project",
-    "author": "Eficent, " "Odoo Community Association (OCA)",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/knowledge",
     "license": "AGPL-3",
     "depends": ["project", "document_page"],
