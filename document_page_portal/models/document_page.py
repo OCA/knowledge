25460cd5aad976597f73aaec6f4c27fedc151f6b
Marcel Savegnago <marcel.savegnago@gmail.com>
2020-02-15 03:43:10 -0300

    [ADD] document_page_portal

---
+++ document_page_portal/models/document_page.py
@@ -0,0 +1,14 @@
+# Copyright 2020 - TODAY, Marcel Savegnago - Escodoo
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class DocumentPage(models.Model):
+
+    _inherit = 'document.page'
+
+    is_public = fields.Boolean(
+        'Public Page',
+        help='If true it allows any user of the portal to have '
+             'access to this document.')
---
+++ document_page_portal/static/src/js/document_page_portal_tour.js
@@ -0,0 +1,37 @@
+odoo.define('document_page_portal.tour', function (require) {
+    'use strict';
+
+    var tour = require("web_tour.tour");
+
+    tour.register('document_page_portal_tour', {
+        test: true,
+        url: '/my',
+    },
+    [
+        {
+            content: "Check document_page_portal is loaded",
+            trigger: 'a[href*="/my/knowledge/documents"]:contains("Knowledge Documents"):first',
+        },
+        {
+            content: "Check public document_page is loaded",
+            trigger: 'a[href*="/knowledge/document/"]:contains("Test Public Page 1"):first',
+        },
+    ]);
+
+    tour.register('document_page_portal_search_tour', {
+        test: true,
+        url: '/my/knowledge/documents',
+    },
+    [
+        {
+            content: "Search",
+            trigger: "input[name='search']",
+            run: "text Test",
+        },
+        {
+            content: "Click Search.",
+            extra_trigger: "#wrap:not(:has(input[name=search]:propValue('')))",
+            trigger: '.search-submit',
+        },
+    ]);
+});
