940bb78363036e48f41fd25cc55a9c07b9b7418a
Denis Roussel <denis.roussel@acsone.eu>
2020-02-27 16:16:20 +0100

    [13.0][MIG] attachment_category"

--- attachment_category/__manifest__.py
+++ attachment_category/__manifest__.py
@@ -5,10 +5,10 @@
     "name": "Atachment Category",
     "summary": """
         Adds a document category to help classification""",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
-    "depends": ["base",],
+    "depends": ["base"],
     "data": [
         "views/ir_attachment.xml",
         "security/ir_attachment_category.xml",
