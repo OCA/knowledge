7b358225347f1e04e6652af4de24786cc9ce37e9
Manuel Calero <manuel.calero@tecnativa.com>
2020-03-11 11:01:29 +0100

    [MIG] document_url: Migration to 13.0

--- document_url/static/src/js/url.js
+++ document_url/static/src/js/url.js
@@ -5,28 +5,28 @@
  * Copyright 2020 Tecnativa - Manuel Calero
  * License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
  */
-odoo.define('document_url', function (require) {
+odoo.define("document_url", function(require) {
     "use strict";
 
-    var AttachmentBox = require('mail.AttachmentBox');
+    var AttachmentBox = require("mail.AttachmentBox");
 
     AttachmentBox.include({
         events: _.extend(AttachmentBox.prototype.events, {
             "click span.o_add_url_button": "_onAddUrl",
         }),
-        _onAddUrl: function () {
-        // Opens wizard to add an URL attachment to the current record
-            this.do_action('document_url.action_ir_attachment_add_url', {
+        _onAddUrl: function() {
+            // Opens wizard to add an URL attachment to the current record
+            this.do_action("document_url.action_ir_attachment_add_url", {
                 additional_context: {
-                    'active_id': this.currentResID,
-                    'active_ids': [this.currentResID],
-                    'active_model': this.currentResModel,
+                    active_id: this.currentResID,
+                    active_ids: [this.currentResID],
+                    active_model: this.currentResModel,
                 },
                 on_close: this._onAddedUrl.bind(this),
             });
         },
-        _onAddedUrl: function () {
-            this.trigger_up('reload_attachment_box');
+        _onAddedUrl: function() {
+            this.trigger_up("reload_attachment_box");
         },
     });
 });
