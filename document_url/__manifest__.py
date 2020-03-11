987769d60847bdae790051d868feb8a196c2476d
Manuel Calero <manuel.calero@tecnativa.com>
2020-03-11 10:28:27 +0100

    [IMP] document_url: black, isort

--- document_url/__manifest__.py
+++ document_url/__manifest__.py
@@ -1,23 +1,15 @@
 # Copyright 2014 Serv. Tecnol. Avanzados (http://www.serviciosbaeza.com)
 #                       Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
 # Copyright 2016 ACSONE SA/NV (<http://acsone.eu>)
+# Copyright 2020 Tecnativa - Manuel Calero
 {
-    'name': 'URL attachment',
-    'version': '12.0.1.0.0',
-    'category': 'Tools',
-    'author': "Tecnativa,"
-              "Odoo Community Association (OCA)",
-    'website': 'https://github.com/OCA/knowledge',
-    'license': 'AGPL-3',
-    'depends': [
-        'document',
-    ],
-    'data': [
-        'view/document_url_view.xml',
-        'security/ir.model.access.csv',
-    ],
-    'qweb': [
-        'static/src/xml/url.xml',
-    ],
-    'installable': True,
+    "name": "URL attachment",
+    "version": "13.0.1.0.0",
+    "category": "Tools",
+    "author": "Tecnativa," "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/knowledge",
+    "license": "AGPL-3",
+    "data": ["view/document_url_view.xml", "security/ir.model.access.csv"],
+    "qweb": ["static/src/xml/url.xml"],
+    "installable": True,
 }
