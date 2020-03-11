03d73407285844c6419375e1a292cf1b45528b6a
Manuel Calero <manuel.calero@tecnativa.com>
2020-03-11 10:28:27 +0100

    [IMP] document_url: black, isort

--- document_url/tests/test_document_url.py
+++ document_url/tests/test_document_url.py
@@ -6,25 +6,24 @@ from odoo.tests import common
 class TestDocumentUrl(common.TransactionCase):
     def setUp(self):
         super(TestDocumentUrl, self).setUp()
-        wizard_add_url = self.env['ir.attachment.add_url']
+        wizard_add_url = self.env["ir.attachment.add_url"]
         ctx = {
-            'active_model': 'res.users',
-            'active_id': self.env.ref('base.user_demo').id,
-            'active_ids': [self.env.ref('base.user_demo').id]
+            "active_model": "res.users",
+            "active_id": self.env.ref("base.user_demo").id,
+            "active_ids": [self.env.ref("base.user_demo").id],
         }
-        self.wizard_add_url = wizard_add_url.with_context(ctx).create({
-            'name': 'Demo User (Website)',
-            'url': 'http://www.odoodemouser.com'
-        })
+        self.wizard_add_url = wizard_add_url.with_context(ctx).create(
+            {"name": "Demo User (Website)", "url": "http://www.odoodemouser.com"}
+        )
 
     def test_add_url_attachment(self):
         self.wizard_add_url.action_add_url()
         domain = [
-            ('type', '=', 'url'),
-            ('name', '=', 'Demo User (Website)'),
-            ('url', '=', 'http://www.odoodemouser.com'),
-            ('res_model', '=', 'res.users'),
-            ('res_id', '=', self.env.ref('base.user_demo').id)
+            ("type", "=", "url"),
+            ("name", "=", "Demo User (Website)"),
+            ("url", "=", "http://www.odoodemouser.com"),
+            ("res_model", "=", "res.users"),
+            ("res_id", "=", self.env.ref("base.user_demo").id),
         ]
-        attachment_added_count = self.env['ir.attachment'].search_count(domain)
+        attachment_added_count = self.env["ir.attachment"].search_count(domain)
         self.assertEqual(attachment_added_count, 1)
