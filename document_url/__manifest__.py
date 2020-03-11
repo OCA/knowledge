c24013bff54451d65ec8491f4239c9b869091de2
Manuel Calero <manuel.calero@tecnativa.com>
2020-03-11 11:01:29 +0100

    [MIG] document_url: Migration to 13.0

--- document_url/__manifest__.py
+++ document_url/__manifest__.py
@@ -1,15 +1,13 @@
-# Copyright 2014 Serv. Tecnol. Avanzados (http://www.serviciosbaeza.com)
-#                       Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
-# Copyright 2016 ACSONE SA/NV (<http://acsone.eu>)
+# Copyright 2014 Tecnativa - Pedro M. Baeza
 # Copyright 2020 Tecnativa - Manuel Calero
 {
     "name": "URL attachment",
     "version": "13.0.1.0.0",
     "category": "Tools",
-    "author": "Tecnativa," "Odoo Community Association (OCA)",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/knowledge",
     "license": "AGPL-3",
-    "data": ["view/document_url_view.xml", "security/ir.model.access.csv"],
+    "data": ["view/document_url_view.xml"],
     "qweb": ["static/src/xml/url.xml"],
     "installable": True,
 }
