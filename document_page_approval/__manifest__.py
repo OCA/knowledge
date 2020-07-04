7c83458f7571eb6a37214361b4f5dd215864a97d
flachica <flachica@gitlab.freyi.es>
2020-07-04 19:11:38 +0200

    [IMP] document_page_approval: black, isort

--- document_page_approval/__manifest__.py
+++ document_page_approval/__manifest__.py
@@ -2,28 +2,25 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Document Page Approval',
-    'version': '12.0.1.0.0',
+    "name": "Document Page Approval",
+    "version": "12.0.1.0.0",
     "author": "Savoir-faire Linux, Odoo Community Association (OCA)",
     "website": "http://www.savoirfairelinux.com",
     "license": "AGPL-3",
-    'category': 'Knowledge Management',
-    'depends': [
-        'document_page',
-        'mail',
+    "category": "Knowledge Management",
+    "depends": ["document_page", "mail",],
+    "data": [
+        "data/email_template.xml",
+        "views/document_page_approval.xml",
+        "security/document_page_security.xml",
+        "security/ir.model.access.csv",
     ],
-    'data': [
-        'data/email_template.xml',
-        'views/document_page_approval.xml',
-        'security/document_page_security.xml',
-        'security/ir.model.access.csv',
+    "images": [
+        "images/category.png",
+        "images/page_history_list.png",
+        "images/page_history.png",
     ],
-    'images': [
-        'images/category.png',
-        'images/page_history_list.png',
-        'images/page_history.png',
-    ],
-    'post_init_hook': 'post_init_hook',
-    'uninstall_hook': 'uninstall_hook',
-    'installable': True,
+    "post_init_hook": "post_init_hook",
+    "uninstall_hook": "uninstall_hook",
+    "installable": True,
 }
