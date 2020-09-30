0594d4b2effe2c98e653b1e09e7c0b028e1aac50
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-09-30 11:39:32 +0200

    [13.0][MIG] document_page_group
    [UPD] Update document_page_group.pot
    
    [UPD] README.rst

--- document_page_group/__manifest__.py
+++ document_page_group/__manifest__.py
@@ -5,10 +5,10 @@
     "name": "Document Page Group",
     "summary": """
         Define access groups on documents""",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "license": "AGPL-3",
     "author": "Creu Blanca,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/knowledge",
-    "depends": ["document_page",],
-    "data": ["security/document_page_security.xml", "views/document_page.xml",],
+    "depends": ["document_page"],
+    "data": ["security/document_page_security.xml", "views/document_page.xml"],
 }
