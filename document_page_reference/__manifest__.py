116b443a72f97adc33eb0e53a029f01251c6940e
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-09-30 11:43:20 +0200

    [IMP] document_page_reference: black, isort, prettier

--- document_page_reference/__manifest__.py
+++ document_page_reference/__manifest__.py
@@ -2,21 +2,18 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Document Page Reference',
-    'summary': """
+    "name": "Document Page Reference",
+    "summary": """
         Include references on document pages""",
-    'version': '12.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'Creu Blanca,Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/knowledge',
-    'depends': [
-        'document_page',
-        'web_editor',
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Creu Blanca,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/knowledge",
+    "depends": ["document_page", "web_editor"],
+    "data": [
+        "views/assets.xml",
+        "views/document_page.xml",
+        "views/report_document_page.xml",
     ],
-    'data': [
-        'views/assets.xml',
-        'views/document_page.xml',
-        'views/report_document_page.xml',
-    ],
-    'maintainers': ['etobella'],
+    "maintainers": ["etobella"],
 }
