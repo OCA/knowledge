46f67a8cac915c8c724a0ef4851621112bf7feea
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-09-30 11:37:18 +0200

    [IMP] document_page_group: black, isort, prettier

--- document_page_group/__manifest__.py
+++ document_page_group/__manifest__.py
@@ -2,18 +2,13 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Document Page Group',
-    'summary': """
+    "name": "Document Page Group",
+    "summary": """
         Define access groups on documents""",
-    'version': '12.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'Creu Blanca,Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/knowledge',
-    'depends': [
-        'document_page',
-    ],
-    'data': [
-        'security/document_page_security.xml',
-        'views/document_page.xml',
-    ],
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Creu Blanca,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/knowledge",
+    "depends": ["document_page",],
+    "data": ["security/document_page_security.xml", "views/document_page.xml",],
 }
