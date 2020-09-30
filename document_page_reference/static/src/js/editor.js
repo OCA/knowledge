b98d1e29d4136ebb38f298feaf75c5800404d230
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-09-30 11:43:20 +0200

    [IMP] document_page_reference: black, isort, prettier

--- document_page_reference/static/src/js/editor.js
+++ document_page_reference/static/src/js/editor.js
@@ -1,37 +1,36 @@
-odoo.define('document_page_reference.backend', function (require) {
-    'use strict';
+odoo.define("document_page_reference.backend", function(require) {
+    "use strict";
 
-    var field_registry = require('web.field_registry');
-    var backend = require('web_editor.backend');
+    var field_registry = require("web.field_registry");
+    var backend = require("web_editor.backend");
     var FieldTextHtmlSimple = backend.FieldTextHtmlSimple;
 
     var FieldDocumentPage = FieldTextHtmlSimple.extend({
         events: _.extend({}, FieldTextHtmlSimple.prototype.events, {
-            'click .oe_direct_line': '_onClickDirectLink',
+            "click .oe_direct_line": "_onClickDirectLink",
         }),
-        _onClickDirectLink: function (event) {
+        _onClickDirectLink: function(event) {
             var self = this;
             event.preventDefault();
             event.stopPropagation();
-            var element = $(event.target).closest('.oe_direct_line')[0];
+            var element = $(event.target).closest(".oe_direct_line")[0];
             var default_reference = element.name;
-            var model = $(event.target).data('oe-model');
-            var id = $(event.target).data('oe-id');
+            var model = $(event.target).data("oe-model");
+            var id = $(event.target).data("oe-id");
             var context = this.record.getContext(this.recordParams);
-            if (default_reference){
-                context['default_reference'] = default_reference
+            if (default_reference) {
+                context.default_reference = default_reference;
             }
             this._rpc({
                 model: model,
-                method: 'get_formview_action',
-                args: [[parseInt(id)]],
+                method: "get_formview_action",
+                args: [[parseInt(id, 10)]],
                 context: context,
-            })
-            .then(function (action) {
-                self.trigger_up('do_action', {action: action});
+            }).then(function(action) {
+                self.trigger_up("do_action", {action: action});
             });
         },
     });
-    field_registry.add('document_page_reference', FieldDocumentPage);
+    field_registry.add("document_page_reference", FieldDocumentPage);
     return FieldDocumentPage;
 });
