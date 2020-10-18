a133da614ad7c93c10c51cb0361ea740d259d155
Denis Roussel <denis.roussel@acsone.eu>
2020-10-18 14:59:51 +0200

    [IMP] : black, isort, prettier

--- attachment_category/__manifest__.py
+++ attachment_category/__manifest__.py
@@ -2,18 +2,16 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Atachment Category',
-    'summary': """
+    "name": "Atachment Category",
+    "summary": """
         Adds a document category to help classification""",
-    'version': '12.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'ACSONE SA/NV,Odoo Community Association (OCA)',
-    'depends': [
-        "base",
-    ],
-    'data': [
-        'views/ir_attachment.xml',
-        'security/ir_attachment_category.xml',
-        'views/ir_attachment_category.xml',
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "depends": ["base",],
+    "data": [
+        "views/ir_attachment.xml",
+        "security/ir_attachment_category.xml",
+        "views/ir_attachment_category.xml",
     ],
 }
