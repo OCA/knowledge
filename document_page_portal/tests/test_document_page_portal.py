979e9aa23a0b5383ef9d20257bd9e9ebd18fed63
Marcel Savegnago <marcel.savegnago@gmail.com>
2021-02-28 17:26:09 -0300

    [MIG] document_page_portal: Migration to 13.0

--- document_page_portal/tests/test_document_page_portal.py
+++ document_page_portal/tests/test_document_page_portal.py
@@ -4,15 +4,17 @@
 import odoo.tests
 
 
-@odoo.tests.tagged('post_install', '-at_install')
-class TestUi(odoo.tests.HttpCase):
+@odoo.tests.tagged("post_install", "-at_install")
+class TestPortalDocumentPage(odoo.tests.HttpCase):
     def test_01_document_page_portal_tour(self):
         # Create a public document
-        self.env['document.page'].create({
-            'name': 'Test Public Page 1',
-            'content': 'Test content',
-            'is_public': True,
-        })
+        self.env["document.page"].create(
+            {
+                "name": "Test Public Page 1",
+                "content": "Test content",
+                "is_public": True,
+            }
+        )
 
         self.phantom_js(
             "/",
@@ -20,14 +22,15 @@ class TestUi(odoo.tests.HttpCase):
             ".run('document_page_portal_tour')",
             "odoo.__DEBUG__.services['web_tour.tour']"
             ".tours.document_page_portal_tour.ready",
-            login="portal"
+            login="portal",
         )
 
+    def test_02_document_page_portal_tour(self):
         self.phantom_js(
             "/",
             "odoo.__DEBUG__.services['web_tour.tour']"
             ".run('document_page_portal_search_tour')",
             "odoo.__DEBUG__.services['web_tour.tour']"
             ".tours.document_page_portal_search_tour.ready",
-            login="portal"
+            login="portal",
         )
