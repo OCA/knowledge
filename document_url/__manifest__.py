91624a791325823e56a412cad69822382056fff6
sergio-teruel <sergio.teruel@tecnativa.com>
2021-03-07 09:34:40 +0100

    [FIX] document_url: Fix module dependencies.
    document_url 13.0.1.0.1

--- document_url/__manifest__.py
+++ document_url/__manifest__.py
@@ -2,11 +2,12 @@
 # Copyright 2020 Tecnativa - Manuel Calero
 {
     "name": "URL attachment",
-    "version": "13.0.1.0.0",
+    "version": "13.0.1.0.1",
     "category": "Tools",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/knowledge",
     "license": "AGPL-3",
+    "depends": ["mail"],
     "data": ["view/document_url_view.xml"],
     "qweb": ["static/src/xml/url.xml"],
     "installable": True,
