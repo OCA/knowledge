45aa86b9642f43245f0b41d4f6296dcafc8a9fb5
Thomas Pot <t.pot@open2bizz.nl>
2021-07-24 13:09:29 +0200

    Updated: new module:   document_page_project

---
+++ document_page_project/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2019 ForgeFlow S.L. (https://www.forgeflow.com) - Lois Rilo
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Document Page Project",
+    "summary": "This module links document pages to projects",
+    "version": "13.0.1.0.1",
+    "development_status": "Production/Stable",
+    "category": "Project",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/knowledge",
+    "license": "AGPL-3",
+    "depends": ["project", "document_page"],
+    "data": ["views/document_page_views.xml", "views/project_project_views.xml"],
+    "installable": True,
+}
