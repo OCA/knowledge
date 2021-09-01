5efdde8269fe0d5ad53c91b0ead488436e46bf83
davidborromeo <david.borromeo@forgeflow.com>
2021-09-01 14:59:52 +0200

    [MIG] document_url: Migration to 14.0
    [MIG] document_url: Migration to 14.0

--- document_url/__manifest__.py
+++ document_url/__manifest__.py
@@ -2,13 +2,16 @@
 # Copyright 2020 Tecnativa - Manuel Calero
 {
     "name": "URL attachment",
-    "version": "13.0.1.0.1",
+    "version": "14.0.1.0.0",
     "category": "Tools",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/knowledge",
     "license": "AGPL-3",
     "depends": ["mail"],
-    "data": ["view/document_url_view.xml"],
+    "data": [
+        "view/document_url_view.xml",
+        "security/ir.model.access.csv",
+    ],
     "qweb": ["static/src/xml/url.xml"],
     "installable": True,
 }
