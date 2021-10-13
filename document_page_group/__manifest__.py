3467bc05cbbc51767002cb2f5761da19a1f215c1
Du-ma <domantas@vialaurea.lt>
2021-10-13 13:37:42 +0000

    [MIG] document_page_group: Migration to 14.0
    [UPD] Update document_page_group.pot
    
    [UPD] README.rst
    
    Update translation files
    
    Updated by "Update PO files to match POT (msgmerge)" hook in Weblate.
    
    Translation: knowledge-14.0/knowledge-14.0-document_page_group
    Translate-URL: https://translation.odoo-community.org/projects/knowledge-14-0/knowledge-14-0-document_page_group/

--- document_page_group/__manifest__.py
+++ document_page_group/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Document Page Group",
     "summary": """
         Define access groups on documents""",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "author": "Creu Blanca,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/knowledge",
