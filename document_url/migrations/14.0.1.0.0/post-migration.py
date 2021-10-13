ef88da1380b64d62220580757db9d0f19c5e20b4
Víctor Martínez <victor.martinez@tecnativa.com>
2021-10-13 17:27:50 +0200

    [IMP] document_url: Visual and functional improvements in UI. Define the add attachment and add URL buttons on the same line. Allow downloading of url type attachments by clicking on the icon or the name. Define the name of the url type attachments as links (allows to open it in a new tab). Hide the download button for url attachments. Force to set mimetype to "application/link" for url attachments. Displays a link icon for url attachments. TT30263

---
+++ document_url/migrations/14.0.1.0.0/post-migration.py
@@ -0,0 +1,16 @@
+# Copyright 2021 Tecnativa - Víctor Martínez
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.logged_query(
+        env.cr,
+        """
+        UPDATE ir_attachment
+        SET mimetype = 'application/link'
+        WHERE type = 'link'
+        """,
+    )
