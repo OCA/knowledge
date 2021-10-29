797016faaa05846452fbfa5694181e63f7cd9579
Jacob Oldfield <jacob@devigami.com>
2021-10-29 13:44:05 +1100

    [MIG] knowledge: Migration to 15.0

--- document_knowledge/__manifest__.py
+++ document_knowledge/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Knowledge",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "author": "OpenERP SA,"
     "MONK Software, "
     "Tecnativa, "
