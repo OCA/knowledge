b1b04a5b5e6b6675e14d7a7d9b1d15a442a9987b
Danny de Jong <ddejong@therp.nl>
2021-12-09 21:10:47 +0100

    [ADD] document_page_tags: Copied module from 10.0

---
+++ document_page_tags/models/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# Copyright 2015-2018 Therp BV <https://therp.nl>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from . import document_page
+from . import document_page_tag
