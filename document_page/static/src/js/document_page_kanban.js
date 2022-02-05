dca546573897d38834243c7943353e473984c695
Florent THOMAS <florent.thomas@mind-and-go.com>
2022-02-05 23:09:02 +0100

    [ADD]fields on category for oragnaising [IMP]Image on category

--- document_page/static/src/js/document_page_kanban.js
+++ document_page/static/src/js/document_page_kanban.js
@@ -1,28 +1,28 @@
-odoo.define('document_page.update_kanban', function (require) {
-    'use strict';
-
-    var core = require('web.core');
-    var Dialog = require('web.Dialog');
-    var KanbanRecord = require('web.KanbanRecord');
-
-    var QWeb = core.qweb;
-    var _t = core._t;
+odoo.define("document_page.update_kanban", function(require) {
+    "use strict";
+    
+    var KanbanRecord = require("web.KanbanRecord");
 
     KanbanRecord.include({
-        //--------------------------------------------------------------------------
+        // --------------------------------------------------------------------------
         // Private
-        //--------------------------------------------------------------------------
+        // --------------------------------------------------------------------------
 
         /**
          * @override
          * @private
          */
-        _openRecord: function () {
-            if (this.modelName === 'document.page' && this.$(".o_document_page_kanban_boxes a").length) {
-                this.$('.o_document_page_kanban_boxes a').first().click();
+        _openRecord: function() {
+            if (
+                this.modelName === "document.page" &&
+                this.$(".o_document_page_kanban_boxes a").length
+            ) {
+                this.$(".o_document_page_kanban_boxes a")
+                    .first()
+                    .click();
             } else {
                 this._super.apply(this, arguments);
             }
-        },
+        }
     });
 });
