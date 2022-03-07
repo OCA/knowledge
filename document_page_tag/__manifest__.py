f738a368eb90314bb67b95113f1fcdf62546b13b
manu <manuel.regidor@sygel.es>
2022-03-07 13:45:59 +0100

    [MIG]document_page_tag: Migration to 13.0

--- document_page_tag/__manifest__.py
+++ document_page_tag/__manifest__.py
@@ -2,14 +2,14 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Document Page Tag",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "author": "Therp BV,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/knowledge",
     "license": "AGPL-3",
     "category": "Knowledge Management",
     "summary": "Allows you to assign tags or keywords to pages and search for "
     "them afterwards",
-    "depends": ["document_page",],
+    "depends": ["document_page"],
     "data": [
         "views/document_page_tag.xml",
         "views/document_page.xml",
