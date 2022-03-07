b080e113fd3f528bf2d0935a9ab0466b5abb66df
manu <manuel.regidor@sygel.es>
2022-03-07 13:41:52 +0100

    [IMP] document_page_tag: black, isort, prettier

--- document_page_tag/models/document_page.py
+++ document_page_tag/models/document_page.py
@@ -1,9 +1,9 @@
 # Copyright 2015-2018 Therp BV <https://therp.nl>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import models, fields
+from odoo import fields, models
 
 
 class DocumentPage(models.Model):
-    _inherit = 'document.page'
+    _inherit = "document.page"
 
-    tag_ids = fields.Many2many('document.page.tag', string='Keywords')
+    tag_ids = fields.Many2many("document.page.tag", string="Keywords")
