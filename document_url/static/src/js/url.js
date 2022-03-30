a76d179ff7f6e70541bdbc7ec26b29c5cf21d7fa
Joel Zilli <joz@adhoc.com.ar>
2022-03-30 15:02:17 -0300

    [IMP] document_url: pre-commit execution

--- document_url/static/src/js/url.js
+++ document_url/static/src/js/url.js
@@ -5,7 +5,7 @@
  * Copyright 2020 Tecnativa - Manuel Calero
  * License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
  */
-odoo.define("document_url", function(require) {
+odoo.define("document_url", function (require) {
     "use strict";
 
     var AttachmentBox = require("mail.AttachmentBox");
@@ -14,7 +14,7 @@ odoo.define("document_url", function(require) {
         events: _.extend(AttachmentBox.prototype.events, {
             "click span.o_add_url_button": "_onAddUrl",
         }),
-        _onAddUrl: function() {
+        _onAddUrl: function () {
             // Opens wizard to add an URL attachment to the current record
             this.do_action("document_url.action_ir_attachment_add_url", {
                 additional_context: {
@@ -25,7 +25,7 @@ odoo.define("document_url", function(require) {
                 on_close: this._onAddedUrl.bind(this),
             });
         },
-        _onAddedUrl: function() {
+        _onAddedUrl: function () {
             this.trigger_up("reload_attachment_box");
         },
     });
