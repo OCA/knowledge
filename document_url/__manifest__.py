c81e4bb429b85d4cd86368ffe421f14b965e9499
Joel Zilli <joz@adhoc.com.ar>
2022-03-30 15:02:17 -0300

    [MIG] document_url: Migration to 15.0

--- document_url/__manifest__.py
+++ document_url/__manifest__.py
@@ -2,13 +2,18 @@
 # Copyright 2020 Tecnativa - Manuel Calero
 {
     "name": "URL attachment",
-    "version": "13.0.1.0.1",
+    "version": "15.0.1.0.0",
     "category": "Tools",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/knowledge",
     "license": "AGPL-3",
     "depends": ["mail"],
     "data": ["view/document_url_view.xml"],
-    "qweb": ["static/src/xml/url.xml"],
+    "assets": {
+        "web.assets_backend": [
+            "document_url/static/src/js/url.js",
+        ],
+        "web.assets_qweb": ["document_url/static/src/xml/url.xml"],
+    },
     "installable": True,
 }
--- document_url/tests/test_document_url.py
+++ document_url/tests/test_document_url.py
@@ -7,14 +7,12 @@ class TestDocumentUrl(common.TransactionCase):
     def setUp(self):
         super(TestDocumentUrl, self).setUp()
         wizard_add_url = self.env["ir.attachment.add_url"]
-        ctx = {
-            "active_model": "res.users",
-            "active_id": self.env.ref("base.user_demo").id,
-            "active_ids": [self.env.ref("base.user_demo").id],
-        }
-        self.wizard_add_url = wizard_add_url.with_context(ctx).create(
-            {"name": "Demo User (Website)", "url": "http://www.odoodemouser.com"}
-        )
+
+        self.wizard_add_url = wizard_add_url.with_context(
+            active_model="res.users",
+            active_id=self.env.ref("base.user_demo").id,
+            active_ids=[self.env.ref("base.user_demo").id],
+        ).create({"name": "Demo User (Website)", "url": "http://www.odoodemouser.com"})
 
     def test_add_url_attachment(self):
         self.wizard_add_url.action_add_url()
