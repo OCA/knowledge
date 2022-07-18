9b0b25a4c937292ead2807ba826ac84ca1f5361a
matiasperalta1 <matias_peralta1@hotmail.com>
2022-07-18 16:33:26 -0300

    [MIG] document_url: Migration to 15.0

--- document_url/__manifest__.py
+++ document_url/__manifest__.py
@@ -2,16 +2,23 @@
 # Copyright 2020 Tecnativa - Manuel Calero
 {
     "name": "URL attachment",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "category": "Tools",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/knowledge",
     "license": "AGPL-3",
     "depends": ["mail"],
     "data": [
-        "view/document_url_view.xml",
         "security/ir.model.access.csv",
+        "view/document_url_view.xml",
     ],
-    "qweb": ["static/src/xml/url.xml"],
+    "assets": {
+        "web.assets_backend": [
+            "document_url/static/src/js/url.esm.js",
+        ],
+        "web.assets_qweb": [
+            "document_url/static/src/xml/url.xml",
+        ],
+    },
     "installable": True,
 }
