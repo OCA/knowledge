5b011488b5fcfd17f2029e833b757d24d437908e
Denis Roussel <denis.roussel@acsone.eu>
2022-08-09 18:24:16 +0200

    [REV] document_page_project: Revert to Beta as document_page is Beta

--- document_page_project/__manifest__.py
+++ document_page_project/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Document Page Project",
     "summary": "This module links document pages to projects",
     "version": "13.0.1.0.1",
-    "development_status": "Production/Stable",
+    "development_status": "Beta",
     "category": "Project",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/knowledge",
