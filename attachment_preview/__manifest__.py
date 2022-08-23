86dcba34996aa446032f8e8ddf34dc41c07f5b02
vancouver29 <vancouver2990@gmail.com>
2022-08-23 11:35:25 +0200

    [IMP] attachment_preview: black, isort, prettier

--- attachment_preview/__manifest__.py
+++ attachment_preview/__manifest__.py
@@ -4,21 +4,16 @@
 {
     "name": "Preview attachments",
     "version": "12.0.1.0.2",
-    "author": "Therp BV,"
-              "Onestein,"
-              "Odoo Community Association (OCA)",
+    "author": "Therp BV," "Onestein," "Odoo Community Association (OCA)",
     "license": "AGPL-3",
-    "summary": 'Preview attachments supported by Viewer.js',
+    "summary": "Preview attachments supported by Viewer.js",
     "category": "Knowledge Management",
-    "depends": [
-        'web',
-        'mail'
-    ],
+    "depends": ["web", "mail"],
     "data": [
         "templates/assets.xml",
     ],
     "qweb": [
-        'static/src/xml/attachment_preview.xml',
+        "static/src/xml/attachment_preview.xml",
     ],
     "installable": True,
 }
