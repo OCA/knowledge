7cadda844128a8929b8cf8fdeb148ec54461f27f
mle <marie.lejeune@acsone.eu>
2022-10-19 11:10:09 +0200

    [MIG] knowledge: Migration to 16.0

--- document_knowledge/__manifest__.py
+++ document_knowledge/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Knowledge",
-    "version": "15.0.1.0.1",
+    "version": "16.0.1.0.0",
     "author": "OpenERP SA,"
     "MONK Software, "
     "Tecnativa, "
