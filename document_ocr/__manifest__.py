f1f13f1e8b41780dc76e30c9b179782078790104
len <nans.lefebvre@gmail.com>
2023-09-07 11:09:24 +0200

    [IMP] document_ocr: pre-commit execution

--- document_ocr/__manifest__.py
+++ document_ocr/__manifest__.py
@@ -1,23 +1,22 @@
-# -*- coding: utf-8 -*-
 # © 2016 Therp BV <http://therp.nl>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "OCR for documents",
-    "version": "8.0.1.0.0",
+    "version": "16.0.1.0.0",
     "author": "Therp BV,Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Knowledge Management",
     "summary": "Run character recognition on uploaded files",
     "depends": [
-        'document',
+        "document",
     ],
     "data": [
         "data/ir_cron.xml",
         "data/ir_config_parameter.xml",
     ],
     "external_dependencies": {
-        'bin': [
-            'tesseract',
+        "bin": [
+            "tesseract",
         ],
     },
 }
