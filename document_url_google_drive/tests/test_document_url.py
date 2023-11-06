c24bdd0159fd256342f14d16b1d5968998170d29
Demchuk Mykola <kolya233445@gmail.com>
2023-11-06 22:18:03 +0200

    [FIX]document_url_google_drive: fix in tests move name parameters

--- document_url_google_drive/tests/test_document_url.py
+++ document_url_google_drive/tests/test_document_url.py
@@ -33,9 +33,7 @@ class TestDocumentUrl(common.TransactionCase):
         self.assertEqual(attachment.mimetype, "application/link")
 
     def test_get_google_picker_params_is_active(self):
-        self.config_settings.set_param(
-            "document_url_google_drive.is_active_google_api", True
-        )
+        self.config_settings.set_param("is_active_google_api", True)
         user = self.users.with_context(no_reset_password=True).create(
             {
                 "name": "Test User",
@@ -62,9 +60,7 @@ class TestDocumentUrl(common.TransactionCase):
         )
 
     def test_get_google_picker_params_not_is_active(self):
-        self.config_settings.set_param(
-            "document_url_google_drive.is_active_google_api", False
-        )
+        self.config_settings.set_param("is_active_google_api", False)
         user = self.users.with_context(no_reset_password=True).create(
             {
                 "name": "Test User",
