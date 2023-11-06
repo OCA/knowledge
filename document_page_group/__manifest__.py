de07e192c76cf0dcb8734f9d6497aab8ba5a1fea
Ivan Sokolov <jzx115@gmail.com>
2023-11-06 19:23:04 +0100

    Updated from template

--- document_page_group/__manifest__.py
+++ document_page_group/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Document Page Group",
     "summary": """
         Define access groups on documents""",
-    "version": "16.0.1.0.0",
+    "version": "16.0.1.0.1",
     "license": "AGPL-3",
     "author": "Creu Blanca,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/knowledge",
