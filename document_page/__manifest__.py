9b210410e8976b01d759517b47d8e0271cbdd6bf
Carolina Fernandez <carolina.fernandez@tecnativa.com>
2024-01-10 10:10:34 -0300

    [16.0][FIX] document_page: Fix quick create and edit form category on pages

--- document_page/__manifest__.py
+++ document_page/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Document Page",
-    "version": "16.0.1.1.1",
+    "version": "16.0.1.2.0",
     "category": "Knowledge Management",
     "author": "OpenERP SA, Odoo Community Association (OCA)",
     "images": [
