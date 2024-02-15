1125d299af5d8efcf6286e02b28f162e9d042779
len <nans.lefebvre@gmail.com>
2024-02-15 11:19:29 +0100

    [ADD] attachment_indexation_textract

---
+++ attachment_indexation_textract/README.rst
@@ -0,0 +1,5 @@
+======================================================
+Attachments List and Document Indexation with Textract
+======================================================
+
+Module to index documents with Textract; install optional dependencies to support more file formats. Supported formats include .xls, .doc with antiword, etc.
---
+++ attachment_indexation_textract/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2023 len-foss/Financial Way
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Attachments Indexation with Textract",
+    "category": "Hidden/Tools",
+    "version": "16.0.1.0.0",
+    "summary": "Attachments List and Document Indexation with PyMuPDF",
+    "author": "len-foss/FinancialWay,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/knowledge",
+    "license": "AGPL-3",
+    "depends": ["attachment_indexation"],
+    "auto_install": True,
+    "installable": True,
+    "data": [],
+    "assets": {},
+    "external_dependencies": {
+        "python": ["textract"],
+        "bin": ["antiword"],
+    },
+}
