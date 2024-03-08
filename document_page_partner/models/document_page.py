90d502a5cf5e96abbc4f98abcffe33b5bcefacdd
ntsirintanis <ntsirintanis@therp.nl>
2024-03-08 09:33:29 +0100

    [MIG] document_page_partner: Migration to 16.0

--- document_page_partner/models/document_page.py
+++ document_page_partner/models/document_page.py
@@ -1,6 +1,6 @@
-# Copyright 2015-18 Therp BV <https://therp.nl>
+# Copyright 2015-24 Therp BV <https://therp.nl>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from openerp import fields, models
+from odoo import fields, models
 
 
 class DocumentPage(models.Model):
