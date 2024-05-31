acf7f9f6185e8c708ff0d4099f7c715c1bdbf1d7
Víctor Martínez <victor.martinez@tecnativa.com>
2024-05-31 12:29:33 +0200

    [ADD] document_page_access_group_user_role: New module
    TT48787

---
+++ document_page_access_group_user_role/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2024 Tecnativa - Víctor Martínez
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Document Page Access Group User Role",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/knowledge",
+    "version": "16.0.1.0.0",
+    "depends": ["document_page_access_group", "base_user_role"],
+    "license": "AGPL-3",
+    "category": "Knowledge",
+    "data": [
+        "views/document_page_views.xml",
+    ],
+    "installable": True,
+    "maintainers": ["victoralmau"],
+}
