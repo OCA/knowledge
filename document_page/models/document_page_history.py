81725ef69882870992c2dc4b58d4cd1321b2ac20
EdgarRetes <edgar.retes24@gmail.com>
2024-10-02 10:40:29 -0600

    [MIG] document_page_approval: Migration to 17.0

--- document_page/models/document_page_history.py
+++ document_page/models/document_page_history.py
@@ -66,11 +66,5 @@ class DocumentPageHistory(models.Model):
                 context=True,
             )
 
-    # TODO: Replace for _compute_display_name
     def name_get(self):
         return [(rec.id, "%s #%i" % (rec.page_id.name, rec.id)) for rec in self]
-
-    @api.depends("page_id")
-    def _compute_display_name(self):
-        for rec in self:
-            rec.display_name = rec.id, "%s #%i" % (rec.page_id.name, rec.id)
