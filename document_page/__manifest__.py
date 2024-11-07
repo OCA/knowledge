196163520eee0085bfe2e3a7555bf0216aea1a45
Justine Doutreloux <justine.doutreloux@gmail.com>
2024-11-07 16:58:52 +0100

    [IMP] : pre-commit auto fixes

--- document_page/__manifest__.py
+++ document_page/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Document Page",
-    "version": "17.0.1.1.0",
+    "version": "18.0.1.0.0",
     "category": "Knowledge Management",
     "author": "OpenERP SA, Odoo Community Association (OCA)",
     "images": [
