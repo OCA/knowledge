893d3bd8a58470c3f8a808b3f01b8435c02c1972
Bhavesh Heliconia <oca@heliconia.in>
2025-02-17 16:41:17 +0530

    [MIG] document_page_access_group: Migration to 18.0

--- document_page_access_group/tests/test_document_page_access_group.py
+++ document_page_access_group/tests/test_document_page_access_group.py
@@ -1,4 +1,5 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import Command
 from odoo.exceptions import UserError
 from odoo.tests.common import users
 
@@ -8,7 +9,7 @@ from .common import TestDocumentPageAccessGroupBase
 class TestDocumentPageAccessGroup(TestDocumentPageAccessGroupBase):
     def test_page_access_constrains(self):
         with self.assertRaises(UserError):
-            self.knowledge_page.write({"user_ids": [(6, 0, [self.user.id])]})
+            self.knowledge_page.write({"user_ids": [Command.set([self.user.id])]})
 
     @users("test-user")
     def test_page_access_01(self):
