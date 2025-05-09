55bee4bbcb94918a6570f1a4cedd6da458f63601
Anusha <anusha.pp@calin.co.in>
2025-05-09 07:38:43 +0200

    [MIG] document_page_reference: Migration to 18.0

--- document_page_reference/__manifest__.py
+++ document_page_reference/__manifest__.py
@@ -5,18 +5,18 @@
     "name": "Document Page Reference",
     "summary": """
         Include references on document pages""",
-    "version": "16.0.1.0.1",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "author": "Creu Blanca,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/knowledge",
-    "depends": ["document_page", "web_editor"],
+    "depends": ["document_page"],
     "data": [
         "views/document_page.xml",
         "views/report_document_page.xml",
     ],
     "assets": {
         "web.assets_backend": [
-            "document_page_reference/static/src/js/**/*",
+            "document_page_reference/static/src/js/editor.js",
         ],
     },
     "maintainers": ["etobella"],
--- document_page_reference/tests/test_document_reference.py
+++ document_page_reference/tests/test_document_reference.py
@@ -18,11 +18,13 @@ class TestDocumentReference(TransactionCase):
             {"name": "Test Page 1", "content": "${r1}", "reference": "r2"}
         )
 
-    def test_constrains_01(self):
+    def test_constraints_duplicate_reference(self):
+        """Should raise if reference is not unique (same as another)."""
         with self.assertRaises(ValidationError):
             self.page2.write({"reference": self.page1.reference})
 
-    def test_constrains_02(self):
+    def test_constraints_invalid_reference(self):
+        """Should raise if reference does not match the required pattern."""
         with self.assertRaises(ValidationError):
             self.page2.write({"reference": self.page2.reference + "-02"})
 
@@ -35,7 +37,7 @@ class TestDocumentReference(TransactionCase):
         self.assertEqual(self.page2.display_name, self.page1.get_raw_content())
 
     def test_check_reference(self):
-        self.assertRegex(self.page1.content_parsed, ".*%s.*" % self.page2.display_name)
+        self.assertRegex(self.page1.content_parsed, f".*{self.page2.display_name}.*")
 
     def test_no_reference(self):
         self.page2.reference = "r3"
@@ -48,31 +50,31 @@ class TestDocumentReference(TransactionCase):
             {"name": "Test Page with no rEfErenCe", "content": "some content"}
         )
         self.assertEqual(new_page.reference, "test_page_with_no_reference")
-        new_page_duplicated_name = self.page_obj.create(
-            {
-                "name": "test page with no reference",
-                "content": "this should have an empty reference "
-                "because reference must be unique",
-            }
-        )
-        self.assertFalse(new_page_duplicated_name.reference)
+        with self.assertRaises(ValidationError):
+            new_page_duplicated_name = self.page_obj.create(
+                {
+                    "name": "test page with no reference",
+                    "content": "this should have an empty reference "
+                    "because reference must be unique",
+                }
+            )
+            self.assertFalse(new_page_duplicated_name.reference)
 
     def test_get_formview_action(self):
         res = self.page1.get_formview_action()
         view_id = self.env.ref("document_page.view_wiki_form").id
-        expected_result = {
+        expected_keys = {
             "type": "ir.actions.act_window",
-            "context": {},
             "res_model": "document.page",
             "res_id": self.page1.id,
-            "view_mode": "form",
-            "view_type": "form",
+            "context": {},
             "target": "current",
             "views": [(view_id, "form")],
         }
-        self.assertEqual(res, expected_result)
+        for key, expected_value in expected_keys.items():
+            self.assertEqual(res.get(key), expected_value, f"Mismatch in key: {key}")
 
     def test_compute_content_parsed(self):
-        self.page1.content = "<p>"
+        self.page1.content = "<p></p>"
         self.page1._compute_content_parsed()
         self.assertEqual(str(self.page1.content_parsed), "<p></p>")
