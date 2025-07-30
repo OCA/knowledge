bd51edf667645697db90de34eb9598fd6566b8b7
Víctor Martínez <victor.martinez@tecnativa.com>
2025-07-30 10:01:52 +0200

    [IMP] document_page: Change t-raw to t-out to prevent warning log

--- document_page/__manifest__.py
+++ document_page/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Document Page",
-    "version": "18.0.1.0.1",
+    "version": "18.0.2.0.0",
     "category": "Knowledge Management",
     "author": "OpenERP SA, Odoo Community Association (OCA)",
     "images": [
