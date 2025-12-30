15353ba9c56fcc3d20b5fc8a6b306b4e93bb608c
Bhavesh Heliconia <oca@heliconia.in>
2025-12-30 09:49:05 +0530

    [MIG] document_page_partner: Migration to 19.0

--- document_page_partner/__manifest__.py
+++ document_page_partner/__manifest__.py
@@ -2,9 +2,9 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Link to a partner in document pages",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "website": "https://github.com/OCA/knowledge",
-    "author": "Therp BV,Odoo Community Association (OCA)",
+    "author": "Therp BV, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Knowledge Management",
     "summary": "Allows to link doucment pages to a partner",
