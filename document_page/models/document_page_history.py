af45aee8c8857663cc9ebcf59138e3a470b486cd
Jan Suhr <jan@nitrokey.com>
2026-04-05 14:37:17 +0200

    [IMP] document_page: Fix tuple assignment error

--- document_page/models/document_page_history.py
+++ document_page/models/document_page_history.py
@@ -54,4 +54,7 @@ class DocumentPageHistory(models.Model):
     @api.depends("page_id")
     def _compute_display_name(self):
         for rec in self:
-            rec.display_name = rec.id, "%s #%i" % (rec.page_id.name, rec.id)
+            if isinstance(rec.id, models.NewId):
+                rec.display_name = rec.page_id.name or ""
+            else:
+                rec.display_name = "%s #%i" % (rec.page_id.name, rec.id)
