023b27867ce23b13d4aedfde4d7e11f8a39e73ad
Víctor Martínez <victor.martinez@tecnativa.com>
2026-04-13 12:16:51 +0200

    [MIG] document_page_product: Migration to 18.0
    TT61666

--- document_page_product/__manifest__.py
+++ document_page_product/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Document Page Product",
     "summary": "This module links document pages to products",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Product",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/knowledge",
